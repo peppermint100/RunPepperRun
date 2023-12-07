451d91bd3bf9d6ecfb6e919aa07235fe152ef916
peppermint100 <krpeppermint100@gmail.com>
2023-12-07 15:15:31 +0900

    init

---
+++ RunPepperRun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunPepperRun
+//
+//  Created by peppermint100 on 12/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
