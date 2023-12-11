8fd4581d573d613e2625524c9a6fa41d4e444707
peppermint100 <krpeppermint100@gmail.com>
2023-12-11 16:34:09 +0900

    RunningResultVC 기본 레이아웃

--- RunPepperRun/Screen/RunningResultViewController.swift
+++ RunPepperRun/Screen/RunningResultViewController.swift
@@ -6,12 +6,71 @@
 //
 
 import UIKit
+import MapKit
 
 class RunningResultViewController: UIViewController {
     
     var route: Route?
+    
+    private let stackView: UIStackView = {
+        let sv = UIStackView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .vertical
+        return sv
+    }()
+    
+    private let resultMapView: MKMapView = {
+        let mv = MKMapView()
+        return mv
+    }()
+    
+    // TODO: - collectionView로 그리드 표현하도록 수정
+    private let runningResultView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let endButtonView: UIView = {
+        let view = UIView()
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        buildUI()
+        applyConstraints()
+    }
+    
+    private func buildUI() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(resultMapView)
+        stackView.addArrangedSubview(runningResultView)
+        stackView.addArrangedSubview(endButtonView)
+    }
+    
+    private func applyConstraints() {
+        let stackViewConstraints = [
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ]
+        
+        let resultMapViewConstraints = [
+            resultMapView.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.4),
+        ]
+        
+        let runningResultViewConstraints = [
+            runningResultView.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.4),
+        ]
+        
+        let endButtonViewConstraints = [
+            endButtonView.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.2),
+        ]
+        
+        NSLayoutConstraint.activate(stackViewConstraints)
+        NSLayoutConstraint.activate(resultMapViewConstraints)
+        NSLayoutConstraint.activate(runningResultViewConstraints)
+        NSLayoutConstraint.activate(endButtonViewConstraints)
     }
 }
