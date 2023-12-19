44822baa3679b3a3a12f391fd4021a23b83948a0
peppermint100 <krpeppermint100@gmail.com>
2023-12-19 15:51:19 +0900

    RunningStatus - RunningStatusCollectionViewCell 레이블 관련 메소드 추가

--- RunPepperRun/Running/RunningStatus.swift
+++ RunPepperRun/Running/RunningStatus.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
-enum RunningStatus: Int {
+
+enum RunningStatus: Int, CaseIterable {
     case speed, pace, distance, caloriesBurned
+    
+    func getTitlesFrom(_ route: Route?) -> RunningStatusTitles {
+        switch self {
+        case .speed:
+            return RunningStatusTitles(title: String(format: "%.2fkm/h", route?.speed ?? "-"), subTitle: "속도")
+        case .pace:
+            let paceMinutes = 1
+            let paceSeconds = 12
+            return RunningStatusTitles(title: String(format: "%d'%d\"", paceMinutes, paceSeconds), subTitle: "페이스")
+        case .distance:
+            let distanceKm = route?.distance.toKiloMeters()
+            return RunningStatusTitles(title: String(format: "%.2fkm/h", distanceKm ?? "-"), subTitle: "거리")
+        case .caloriesBurned:
+            return RunningStatusTitles(title: String(format: "%.2fcal", route?.caloriesBurned ?? "-"), subTitle: "소모 칼로리")
+        }
+    }
 }
---
+++ RunPepperRun/Running/RunningStatusTitles.swift
@@ -0,0 +1,13 @@
+//
+//  RunningStatusTitles.swift
+//  RunPepperRun
+//
+//  Created by peppermint100 on 12/19/23.
+//
+
+import Foundation
+
+struct RunningStatusTitles {
+    let title: String
+    let subTitle: String
+}
