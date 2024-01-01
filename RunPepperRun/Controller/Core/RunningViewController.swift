6345c3498e04371311be74a72e1071ebaf5904ae
peppermint100 <krpeppermint100@gmail.com>
2024-01-01 20:29:22 +0900

    build - setup 함수명 변경

--- RunPepperRun/Controller/Core/RunningViewController.swift
+++ RunPepperRun/Controller/Core/RunningViewController.swift
@@ -7,121 +7,46 @@
 
 import UIKit
 import MapKit
-
+import SnapKit
 
 class RunningViewController: UIViewController {
     
     private let locationManager = CLLocationManager()
     
-    private let stackView: UIStackView = {
-        let sv = UIStackView()
-        sv.translatesAutoresizingMaskIntoConstraints = false
-        sv.axis = .vertical
-        return sv
-    }()
-    
-    private let runningCardView: UICollectionView = {
-        let layout = RunningCardCollectionViewFlowLayout()
-        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    private let runningMapView: MKMapView = {
+    private let mapView: MKMapView = {
         let mv = MKMapView()
         mv.translatesAutoresizingMaskIntoConstraints = false
         mv.userTrackingMode = .follow
+        mv.showsUserLocation = true
         return mv
-    }()
-    
-    private let startButton: RoundedButton = {
-        let button = RoundedButton("시작", color: .systemBlue, shadow: true)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        buildUI()
-        buildNavigationBar()
-        buildCollectionView()
+        setupUI()
         applyConstraints()
-        setUpLocationManager()
-        setUpStartButton()
+        setupNavigationBar()
+        setupLocationManager()
         handleLocationAuthorization()
     }
     
 // MARK: - 네비게이션 바 세팅
-    private func buildNavigationBar() {
+    private func setupNavigationBar() {
         navigationItem.title = "러닝"
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
 // MARK: - UI 세팅
-    private func buildUI() {
+    private func setupUI() {
         view.backgroundColor = .systemBackground
-        view.addSubview(stackView)
-        view.addSubview(startButton)
-        stackView.addArrangedSubview(runningCardView)
-        stackView.addArrangedSubview(runningMapView)
+        view.addSubview(mapView)
     }
-    
-    private func buildCollectionView() {
-        runningCardView.delegate = self
-        runningCardView.dataSource = self
-        runningCardView.register(RunningCardCollectionViewCell.self, forCellWithReuseIdentifier: RunningCardCollectionViewCell.identifier)
-    }
-    
-//MARK: - 제약조건
-    private func applyConstraints() {
-        let stackViewConstraints = [
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ]
-        
-        let runningHistoryViewConstraints = [
-            runningCardView.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.35)
-        ]
-        
-        let runningMapViewConstraints = [
-            runningMapView.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.65)
-        ]
-        
-        let startButtonConstraints = [
-            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            startButton.bottomAnchor.constraint(equalTo: runningMapView.bottomAnchor, constant: -30),
-            startButton.widthAnchor.constraint(equalTo: runningMapView.widthAnchor, multiplier: 0.3),
-            startButton.heightAnchor.constraint(equalTo: runningMapView.widthAnchor, multiplier: 0.3),
-        ]
-        
-        NSLayoutConstraint.activate(stackViewConstraints)
-        NSLayoutConstraint.activate(runningHistoryViewConstraints)
-        NSLayoutConstraint.activate(runningMapViewConstraints)
-        NSLayoutConstraint.activate(startButtonConstraints)
-    }
+
 }
 
-// MARK: - 컬렉션 뷰 델리게이트
-extension RunningViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView
-            .dequeueReusableCell(withReuseIdentifier: RunningCardCollectionViewCell.identifier, for: indexPath)
-                as? RunningCardCollectionViewCell else { return UICollectionViewCell() }
-        
-        return cell
-    }
-}
-
-    
 // MARK: - 맵, 위치 관련
 extension RunningViewController: CLLocationManagerDelegate, MKMapViewDelegate {
-    private func setUpLocationManager() {
+    private func setupLocationManager() {
         locationManager.delegate = self
     }
     
@@ -139,28 +64,5 @@ extension RunningViewController: CLLocationManagerDelegate, MKMapViewDelegate {
         default:
             break
         }
-    }
-}
-
-// MARK: - 시작 버튼 관련
-extension RunningViewController {
-    private func setUpStartButton() {
-        startButton.addTarget(self, action: #selector(tapStartButton), for: .touchUpInside)
-    }
-    
-    @objc private func tapStartButton() {
-        switch locationManager.authorizationStatus {
-        case .authorizedAlways, .authorizedWhenInUse:
-            presentToTrackingVC()
-        default:
-            // TODO: - 권한 재요청 코드 추가
-            break
-        }
-    }
-    
-    private func presentToTrackingVC() {
-        let vc = TrackingViewController()
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: false)
     }
 }
