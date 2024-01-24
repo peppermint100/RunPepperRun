48d53a0cec8b0b59101a67c9fa98c86aadc0aedf
peppermint100 <krpeppermint100@gmail.com>
2024-01-24 17:04:46 +0900

    [Refactor] UserManager, FirebaseRepository 분리

--- RunPepperRun/Model/UserManager.swift
+++ RunPepperRun/Model/UserManager.swift
@@ -12,38 +12,22 @@ import FirebaseFirestore
 
 class UserManager {
     static let shared = UserManager()
-    let userRef = Firestore.firestore().collection(FirestoreCollection.user.rawValue)
+    
+    let repository = FirestoreRepository.shared
     
     func getEmail() -> String? {
-        let currentUser = Auth.auth().currentUser
-        return currentUser?.email
+        return repository.getEmail()
     }
     
     func createUser(nickname: String, weight: Double, completion: ((Bool) -> Void)?) {
-        guard let email = getEmail() else {
-            completion?(false)
-            return
-        }
-        
-        let user = User(nickname: nickname, weight: weight)
-        
-        do {
-            try userRef.document(email).setData(from: user)
-            completion?(true)
-        } catch {
-            completion?(false)
-            print("유저 정보를 Firebase에 저장하는데 실패했습니다.")
+        repository.createUser(User(nickname: nickname, weight: weight)) { success in
+            completion?(success)
         }
     }
     
     func exists(email: String, completion: @escaping (Bool) -> Void) {
-        userRef.document(email).getDocument { document, error in
-            guard error == nil, let document = document else {
-                completion(false)
-                return
-            }
-            
-            completion(document.exists)
+        repository.exists(email: email) { exists in
+            completion(exists)
         }
     }
 }
